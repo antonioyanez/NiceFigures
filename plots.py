f4e52fe77cc85c7306e2a8cc449152dcf9ba9466
Antonio Yanez <antonio.yanez.santamaria@gmail.com>
2017-03-20 16:41:24 +0100

    adjusted margins

--- plots.py
+++ plots.py
@@ -1,13 +1,17 @@
 import sys; sys.path.insert(0,'/usr/local/lib/python2.7/dist-packages')
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
-fig = plt.figure(figsize=(6.9,12))
-ax = fig.add_subplot(1,1,1)
+gs = gridspec.GridSpec(1,1,bottom=.08,left=.11,top=.99,right=.99)
+fig = plt.figure(figsize=(6.25,6.3))
+ax = fig.add_subplot(gs[0,0])
 
 x = np.linspace(-10,10,1001)
 
 ax.plot(x,np.sin(2*np.pi*x/2)/x)
+ax.set_ylabel('V (mV)')
+ax.set_xlabel('t (ms)')
 
 
 fig.savefig('figure.pdf')
